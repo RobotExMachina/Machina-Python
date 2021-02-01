7e924ac8ee955f8112eff1ff2da27f27378d0cc3
DESKTOP-JLX <personal@garciadelcastillo.es>
2021-01-31 22:41:00 -0500

    TransformTo for sample file

--- samples/machina_python_sample.py
+++ samples/machina_python_sample.py
@@ -4,7 +4,7 @@ def main():
     bot = MachinaRobot()
     bot.Message("Hello Robot!")
     bot.SpeedTo(100)
-    bot.MoveTo(400,300,500)
+    bot.TransformTo(400,300,500,-1,0,0,0,1,0)
     bot.Rotate(0,1,0,-90)
     bot.Move(0,0,250)
     bot.Wait(2000)
